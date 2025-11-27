ad15cd0ef0620d7b5e9fde53e011426f9d499693
Fox Scarlett <foxontheone@gmail.com>
2025-11-27 00:02:55 -0800

    clerk setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
 import ThemeComp from '@/components/ThemeComp'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,13 +26,15 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
-      <body>
-        <ThemeComp>
-          <Header />
-          {children}
-        </ThemeComp>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang='en' suppressHydrationWarning>
+        <body>
+          <ThemeComp>
+            <Header />
+            {children}
+          </ThemeComp>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
