be76629739fa45415430d2c6519def9c9ae142a2
Fox Scarlett <foxontheone@gmail.com>
2026-01-30 20:43:47 -0800

    placeholders for releases, news, outsidehap

--- src/app/outside-hap/page.tsx
+++ src/app/outside-hap/page.tsx
@@ -1,7 +1,80 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function OutsideHapPage() {
   return (
     <main>
       <h1>Outside HAP</h1>
+      <div className='flex flex-col gap-4 mt-18 mb-18'>
+        <ul>
+          <li>
+            <p className='text-xl mt-4'>
+              <Link href='/' className='flex flex-row items-start gap-4'>
+                <Image
+                  src='/hap-logo-rip.png'
+                  alt='Bizzartek Mixing'
+                  width={50}
+                  height={50}
+                />
+                cool release 1
+              </Link>
+            </p>
+          </li>
+          <li>
+            <p className='text-xl mt-4'>
+              <Link href='/' className='flex flex-row items-start gap-4'>
+                <Image
+                  src='/hap-logo-rip.png'
+                  alt='Its Gonna Crash'
+                  width={50}
+                  height={50}
+                />
+                cool release 2
+              </Link>
+            </p>
+          </li>
+          <li>
+            <p className='text-xl mt-4'>
+              <Link href='/' className='flex flex-row items-start gap-4'>
+                <Image
+                  src='/hap-logo-rip.png'
+                  alt='Etacarina'
+                  width={50}
+                  height={50}
+                />
+                cool release 3
+              </Link>
+            </p>
+          </li>
+
+          <li>
+            <p className='text-xl mt-4'>
+              <Link href='/' className='flex flex-row items-start gap-4'>
+                <Image
+                  src='/hap-logo-rip.png'
+                  alt='Peek photo'
+                  width={50}
+                  height={50}
+                />
+                cool release 4
+              </Link>
+            </p>
+          </li>
+          <li>
+            <p className='text-xl mt-4'>
+              <Link href='/' className='flex flex-row items-start gap-4'>
+                <Image
+                  src='/hap-logo-rip.png'
+                  alt='FourPointZero'
+                  width={50}
+                  height={50}
+                />
+                cool release 5
+              </Link>
+            </p>
+          </li>
+        </ul>
+      </div>
     </main>
   )
 }
